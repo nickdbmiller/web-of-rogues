bbe2cb910ef0fcaea3dce383846cc96f1c7873e1
nickdbmiller <nickbarrettmiller@gmail.com>
2021-12-13 13:56:14 -0500

    Creates event listeners for arrow key presses.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,7 +7,20 @@ const url = `${DOMAIN}${GENERATOR}?width=80&height=25`;
 
 // Set placeholder
 
-// Event listeners
+// Arrow Key Listeners (can be expanded to use WASD and num keys as well.)
+// `https://stackoverflow.com/questions/5597060/detecting-arrow-key-presses-in-javascript/5597114`
+document.onkeydown = checkKey;
+function checkKey(e) {
+    if (e.keyCode == '37') {
+        console.log("Left");
+    } else if (e.keyCode == '38') {
+        console.log("Up");
+    } else if (e.keyCode == '39') {
+        console.log("Right");
+    } else if (e.keyCode == '40') {
+        console.log("Down");
+    };
+};
 
 // Function to call API
 
