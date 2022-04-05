fedd5a110d0f488bff973a19c1adcafed1b3d868
nickdbmiller <nickbarrettmiller@gmail.com>
2022-04-04 20:53:56 -0400

    Impliment React router.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
@@ -8,7 +9,9 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
