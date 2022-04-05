b7e477b925d0125a3188c7374da9745733696a64
nickdbmiller <nickbarrettmiller@gmail.com>
2022-04-04 20:53:17 -0400

    Layout and routes to homepage and game.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout.jsx';
+import { LandingScreen, GameScreen } from './screens/screenIndex';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<LandingScreen />} />
+                    <Route path="/game" element={<GameScreen />} />
+                </Routes>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
